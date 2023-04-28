a15ecc44a6e15126d6c1a7b943edc3a27fe40ff5
cam273364 <cam273364@gmail.com>
2023-04-28 12:56:46 -0400

    "added functionality to add notes and to delete notes"

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fs = require('fs')
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require('uuid');
 app.use(express.static("public"));
 //Below lines need to be added to have access to req.body with post request.
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +24,6 @@ app.get('/api/notes', function (req, res) {
     
     fs.readFile("./db/db.json", "utf8", function (err, data){
         
-        console.log("from get", data)
         res.json(JSON.parse(data))
     })
     
@@ -37,9 +36,10 @@ app.post('/api/notes', function (req, res) {
         console.log(data)
         let notes = JSON.parse(data)
         console.log(notes)
+        req.body.id = uuid()
         notes.push(req.body)
         
-        fs.writeFile("./db/db.json", JSON.stringify(data),{encoding: "utf8"}, function (err, data) {
+        fs.writeFile("./db/db.json", JSON.stringify(notes),{encoding: "utf8"}, function (err, data) {
             if (err) {
                 console.log(err)
             } else {
@@ -52,6 +52,30 @@ app.post('/api/notes', function (req, res) {
         
     })
     
+})
+
+app.delete('/api/notes/:id', function (req, res) {
+    console.log(req.params.id)
+    
+    //cannot add object to array in file - first get array from the file which ive done already, then add object to array, then rewrite file with new array.
+    fs.readFile("./db/db.json", "utf8", function (err, data){
+        
+        let notes = JSON.parse(data)
+        console.log(notes)
+        notes = notes.filter(note => note.id !== req.params.id)
+        
+        fs.writeFile("./db/db.json", JSON.stringify(notes),{encoding: "utf8"}, function (err, data) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('note add successfully')
+            }
+            res.send(req.body)
+        } )
+
+
+        
+    })
 })
 
 app.get('/*', function (req, res) {
